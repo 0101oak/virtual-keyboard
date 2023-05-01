0430770bdf9a3c0d331215d8251c97569cb7182e
0101oak <115372729+0101oak@users.noreply.github.com>
2023-05-01 22:05:52 +0300

    feat: add row 1

--- development/src/index.js
+++ development/src/index.js
@@ -1,5 +1,7 @@
 import './styles.css';
 
+const lang = 0;
+
 const keyboardWrapper = document.createElement('div');
 keyboardWrapper.classList.add('keyboard-wrapper');
 
@@ -33,6 +35,24 @@ keyboard.appendChild(row);
 const array = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backscape'];
 const char = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8];
 
-for (let i = 0; i < 14; i++) {
+for (let i = 0; i < array.length; i++) {
   row.append(Key(array[i], char[i]));
+}
+
+const rowFirst = document.createElement('div');
+row.classList.add('row_1');
+keyboard.appendChild(rowFirst);
+
+const arrayFirst = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\/', 'Del'];
+const charFirst = [9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 46];
+const arrayRusFirst = ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\/', 'Del'];
+
+if (lang === 1) {
+  for (let i = 0; i < arrayFirst.length; i++) {
+    rowFirst.append(Key(arrayFirst[i], charFirst[i]));
+  }
+} else {
+  for (let i = 0; i < arrayRusFirst.length; i++) {
+    rowFirst.append(Key(arrayRusFirst[i], charFirst[i]));
+  }
 }
